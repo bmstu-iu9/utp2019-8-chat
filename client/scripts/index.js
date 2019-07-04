777055e3faa01524c479c0b3bc0efbfa5bdf98b1
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-07-04 19:29:47 +0300

    Update message sending

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,24 +1,51 @@
 const msgTextbox = document.getElementById("input_msg");
+const chatFlow = document.getElementById("chat_flow");
 
-document.getElementById("send_btn").addEventListener("click", (sender) => {
+const addMessage = (author, text) => { //Add message to chat-flow zone
+    chatFlow.innerHTML +=
+        `<div class="msg_box">
+            <div class="name">${author}</div>
+            <div class="msg">${text}</div>
+        </div>`;
+}
+
+const sendMessage = () => {
+    if (msgTextbox.value == "")
+        return;
+    let msg = msgTextbox.value;
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/send_message', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(`channel_id=1&message=${msgTextbox.value}`);
+    xhr.send(`channel_id=1&message=${msg}`);
+    msgTextbox.value = "";
     xhr.onreadystatechange = () => {
         if (xhr.readyState != 4) 
             return;
         if (xhr.status == 200) { //Ok
-            alert(xhr.responseText)
+            addMessage("You", msg);
+            document.getElementById("chat_flow").scrollTop = 9999;
         }
         else if (xhr.status == 401) { //Unauthorized
-            alert("Unauthorized")
+            alert("Unauthorized");
         }
         else if (xhr.status == 403) { //Forbiden
-            alert("Forbiden")
+            alert("Forbiden");
         }
         else {
-            alert("Something weird happened")
+            alert("Something weird happened");
         }
     }
+}
+
+//Example
+addMessage("Alice", "Meaningful message");
+addMessage("Bob", "Strange message");
+addMessage("Alice", "Funny message");
+//End example
+
+
+document.getElementById("send_btn").addEventListener("click", (sender) => sendMessage());
+msgTextbox.addEventListener("keyup", (sender) => {
+    if (sender.key == "Enter")
+        sendMessage();
 });
