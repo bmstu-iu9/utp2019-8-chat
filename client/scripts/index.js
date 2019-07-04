4c0ddd899e1c3ebb2f209ce332c85a610165bfaa
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-07-04 16:22:45 +0300

    Raw method of sending messages

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,1 +1,24 @@
-console.log("Script")
+const msgTextbox = document.getElementById("input_msg");
+
+document.getElementById("send_btn").addEventListener("click", (sender) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/send_message', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(`channel_id=1&message=${msgTextbox.value}`);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState != 4) 
+            return;
+        if (xhr.status == 200) { //Ok
+            alert(xhr.responseText)
+        }
+        else if (xhr.status == 401) { //Unauthorized
+            alert("Unauthorized")
+        }
+        else if (xhr.status == 403) { //Forbiden
+            alert("Forbiden")
+        }
+        else {
+            alert("Something weird happened")
+        }
+    }
+});
