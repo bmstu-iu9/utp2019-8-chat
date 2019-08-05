7acc63debdf7e61742fd540bdbc2b31a418728da
Ga1ath <52465270+Ga1ath@users.noreply.github.com>
2019-08-05 19:07:25 +0300

    Update database.js

--- modules/database.js
+++ modules/database.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const readline = require("readline");
 
 let UsersData = [];
-let UsersChannels = [];
+let UsersChannels = [false];
 
 //Function for creating session key
 const makeSessionKey = () => {
@@ -15,9 +15,18 @@ const makeSessionKey = () => {
     return result;
 }
 
+//Function for checking session key
+const checkSessionKey = (key) => {
+	let i = 0;
+    let len = UsersData.length;
+    for (; i < len; i++) if (UsersData[i].key === key) break;
+    if (i === len) return false;
+	return i;
+}
+
 //Registration new user
-module.exports.registration = (login, password) => {
-    UsersData.push({login: login, password: password, key: "", channels: []});
+module.exports.registration = (login, password, name) => {
+    UsersData.push({login: login, password: password, key: "", channels: [], author_name: name, id: UsersData.length});
 }
 
 //Authentification user
@@ -39,66 +48,57 @@ module.exports.authentication = (login, password) => {
 
 //Shows all channels, available for user
 module.exports.channels_list = (key) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+	let i = checkSessionKey(key);
+    if (i === false) return false;
+
     return UsersData[i].channels;
 }
 
 //Adds channel for user
 module.exports.channels_add = (key, id) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
     UsersData[i].channels.push(id);
+	return true;
 }
 
 //Removes channell from user (not deleting channel for all users, only for one user)
 module.exports.channels_remove = (key, id) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
     let cur = UsersData[i];
     for (i = 0; i < cur.channels.length; i++) if (cur.channels[i] === id) {
         cur.channels[i] = false;
         break;
     }
+    return true;
 }
 
 //Creating new channel (available for (almost)any user)
 module.exports.channels_create = (key, name) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
-    UsersChannels.push({id: UsersChannels.length, name: name, messages: [false]});
-    return UsersChannels.length;
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
+    UsersChannels.push({id: UsersChannels.length, name: name, messages: []});
+    return UsersChannels.length - 1;
 }
 
 //Deleting channel for ALL users
 module.exports.channels_delete = (key, id) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
     UsersChannels[id] = false;
+	return true;
 }
 
 //Returns chat history for a channel
 module.exports.chat_history = (key, id, count, offset) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
     let start = 0;
     let end = 0;
     len = UsersChannels[id].messages.length;
@@ -106,46 +106,51 @@ module.exports.chat_history = (key, id, count, offset) => {
         end = len - offset;
         if (offset + count < len) start = len - offset - count;
     }
-    return [count, UsersChannels[id].messages.slice(start, end)];
+    //Returning number of messages and themselves messages
+    return [end - start, UsersChannels[id].messages.slice(start, end)];
 }
 
 //Sending message for a channel
 module.exports.send_message = (key, id, message) => {
-    let i = 0;
-    let len = UsersData.length;
-    for (; i < len; i++) if (UsersData[i].key === key) break;
-    if (i === len) return false;
-    
+    let i = checkSessionKey(key);
+    if (i === false) return false;
+
     UsersChannels[id].messages.push({message_id: UsersChannels[id].messages.length, author_id: UsersData[i].id, author_name: UsersData[i].author_name, message: message});
     return UsersChannels[id].messages[UsersChannels[id].messages.length - 1];
 }
 
-//Information inside UsersData and UsersChannels, which accumulates during server's session, 
+//Information inside UsersData and UsersChannels, which accumulates during server's session,
 //saves into UsersData.json and UsersChannels.json accordingly
 module.exports.save = () => {
-    fs.writeFileSync("/Data/UsersData.json", JSON.stringify(UsersData[0]));
+    if (UsersData.length > 0) {
+		fs.writeFileSync("./Data/UsersData.json", JSON.stringify(UsersData[0]));
+		fs.appendFileSync("./Data/UsersData.json", "\n");
+	}
     for (let i = 1; i < UsersData.length; i++) {
-        if (i < UsersData.length - 1) fs.appendFileSync("/Data/UsersData.json", "\n");
-        fs.appendFileSync("/Data/UsersData.json", JSON.stringify(UsersData[i]));
+        fs.appendFileSync("./Data/UsersData.json", JSON.stringify(UsersData[i]));
+		if (i < UsersData.length - 1) fs.appendFileSync("./Data/UsersData.json", "\n");
     }
-    fs.writeFileSync("/Data/UsersChannels.json", JSON.stringify(UsersChannels[0]));
+    if (UsersChannels.length > 0) {
+		fs.writeFileSync("./Data/UsersChannels.json", JSON.stringify(UsersChannels[0]));
+		fs.appendFileSync("./Data/UsersChannels.json", "\n");
+	}
     for (let i = 1; i < UsersChannels.length; i++) {
-        if (i < UsersChannels.length - 1) fs.appendFileSync("/Data/UsersChannels.json", "\n");
-        fs.appendFileSync("/Data/UsersChannels.json", JSON.stringify(UsersChannels[i]));
+        fs.appendFileSync("./Data/UsersChannels.json", JSON.stringify(UsersChannels[i]));
+		if (i < UsersChannels.length - 1) fs.appendFileSync("./Data/UsersChannels.json", "\n");
     }
 }
 
-//Loading information, that had been recording during previous server's sessions, 
+//Loading information, that had been recording during previous server's sessions,
 //from UsersData.json and UsersChannels.json to UsersData and UsersChannels respectively
-module.exports.load = () => {
-    let current = readline.createInterface({input: fs.createReadStream("/Data/UsersData.json")});
-    current.on('line', function(line) {
-        UsersData.push(JSON.parse(line));
+module.exports.load = (callback) => {
+    let current = readline.createInterface({input: fs.createReadStream("./Data/UsersData.json")});
+    current.on('line', (line) => {
+        if (line.length > 5) UsersData.push(JSON.parse(line));
     });
     current.on('close', () => {
-        let current_1 = readline.createInterface({input: fs.createReadStream("/Data/UsersChannels.json")});
-        current_1.on('line', function(line) {
-            UsersChannels.push(JSON.parse(line));
+        let current_1 = readline.createInterface({input: fs.createReadStream("./Data/UsersChannels.json")});
+        current_1.on('line', (line) => {
+            if (line.length > 5) UsersChannels.push(JSON.parse(line));
         });
         current_1.on('close', () => {
             callback();
