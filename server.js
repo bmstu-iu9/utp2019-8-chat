0fbf94bdb2ed4fac827815b575933daa6c7fd1a6
Khripach Georgy <haosfortum.random@gmail.com>
2019-08-12 20:51:32 +0300

    Number of lines cutback + Minor fixes

--- server.js
+++ server.js
@@ -15,306 +15,109 @@ const chatModule = require("./modules/chat");
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+//checking API methods arguments function
+const checkArg = (response, arg) => {
+    if (request.body[arg] === undefined) {
+        response.status(200).send(JSON.stringify({
+            success: false,
+            err_code: 1,
+            err_cause: `Argument not found (${arg})`
+        }));
+    }
+    else
+        return request.body[arg];
+}
+
 //API methods
 app.post("/api/register", urlencodedParser, (request, response) => {
-	let login, password;
-	login = request.body.login;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (login)`
-		}));
-	}
-	password = request.body.password;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (password)`
-		}));
-	}
+	let login = checkArg(response, "login");
+    let password = checkArg(response, "password");
+    response.status(200).send("test_REGISTER_method");
 });
 
 app.post("/api/auth", urlencodedParser, (request, response) => {
-	let login, password;
-	login = request.body.login;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (login)`
-		}));
-	}
-	password = request.body.password;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (password)`
-		}));
-	}
+	let login = checkArg(response, "login");
+    let password = checkArg(response, "password");
+    response.status(200).send("test_AUTH_method");
 });
 
 app.post("/api/get_user", urlencodedParser, (request, response) => {
-	let id;
-	id = request.body.id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (id)`
-		}));
-	}
+    let id = checkArg(response, "id");
+    response.status(200).send("test_GET_USER_method");
 });
-/*
-app.post("/api/channels_list", urlencodedParser, (request, response) => {
-	response.status(200).send("test_CHANNELS_LIST_method");
-});
-*/
+
 app.post("/api/add_to_channel", urlencodedParser, (request, response) => {
-	let token, user_id, channel_id;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	user_id = request.body.user_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (user_id)`
-		}));
-	}
-	channel_id = request.body.channel_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_id)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let user_id = checkArg(response, "user_id");
+    let channel_id = checkArg(response, "channel_id");
+    response.status(200).send("test_ADD_TO_CHANNEL_method");
 });
 
 app.post("/api/remove_from_channel", urlencodedParser, (request, response) => {
-	let token, user_id, channel_id;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	user_id = request.body.user_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (user_id)`
-		}));
-	}
-	channel_id = request.body.channel_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_id)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let user_id = checkArg(response, "user_id");
+    let channel_id = checkArg(response, "channel_id");
+    response.status(200).send("test_REMOVE_FROM_CHANNEL_method");
 });
 
 app.post("/api/change_avatar", urlencodedParser, (request, response) => {
-	let token, user_id, avatar;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	user_id = request.body.user_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (user_id)`
-		}));
-	}
-	avatar = request.body.avatar;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (avatar)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let user_id = checkArg(response, "user_id");
+    let avatar = checkArg(response, "avatar");
+    response.status(200).send("test_CHANGE_AVATAR_method");
 });
 
 app.post("/api/change_meta", urlencodedParser, (request, response) => {
-	let token, user_id, meta;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	user_id = request.body.user_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (user_id)`
-		}));
-	}
-	meta = request.body.meta;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (meta)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let user_id = checkArg(response, "user_id");
+    let meta = checkArg(response, "meta");
+    response.status(200).send("test_CHANGE_META_method");
 });
 
 app.post("/api/get_channel", urlencodedParser, (request, response) => {
-	let id;
-	id = request.body.id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (id)`
-		}));
-	}
-	chatModule.addListener(id);
+	let id = checkArg(response, "id");
+    response.status(200).send("test_GET_CHANNEL_method");
 });
 
 app.post("/api/create_channel", urlencodedParser, (request, response) => {
-	let token, channel_name;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	channel_name = request.body.channel_name;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_name)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let channel_name = checkArg(response, "channel_name");
+    response.status(200).send("test_CREATE_CHANNEL_method");
 });
 
 app.post("/api/delete_channel", urlencodedParser, (request, response) => {
-	let token, channel_id;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	channel_id = request.body.channel_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_id)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let channel_id = checkArg(response, "channel_id");
+    response.status(200).send("test_DELETE_CHANNEL_method");
 });
 
 app.post("/api/get_messages", urlencodedParser, (request, response) => {
-	let token, channel_id, offset, count;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	channel_id = request.body.channel_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_id)`
-		}));
-	}
-	offset = request.body.offset;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (offset)`
-		}));
-	}
-	count = request.body.count;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (count)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let channel_id = checkArg(response, "channel_id");
+    let offset = checkArg(response, "offset");
+    let count = checkArg(response, "count");
+    response.status(200).send("test_GET_NESSAGES_method");
 });
 
 app.post("/api/send_message", urlencodedParser, (request, response) => {
-	let token, channel_id, message;
-	token = request.body.token;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (token)`
-		}));
-	}
-	channel_id = request.body.channel_id;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (channel_id)`
-		}));
-	}
-	message = request.body.message;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (message)`
-		}));
-	}
+	let token = checkArg(response, "token");
+    let channel_id = checkArg(response, "channel_id");
+    let message = checkArg(response, "message");
+    response.status(200).send("test_SEND_MESSAGE_method");
 });
 
 app.post("/api/listen", urlencodedParser, (request, response) => {
-    let token, channel_id, last_msg;
-    token = request.body.token;
-    if (token === undefined) {
-        response.status(200).send(JSON.stringify({
-            err_code: 1, //Wrong number of arguments
-            err_cause: `Argument not found (token)`
-        }));
-    }
-    channel_id = request.body.channel_id;
-    if (channel_id === undefined) {
-        response.status(200).send(JSON.stringify({
-            err_code: 1, //Wrong number of arguments
-            err_cause: `Argument not found (channel_id)`
-        }));
-    }
-    last_msg = request.body.last_msg;
-    if (last_msg === undefined) {
-        response.status(200).send(JSON.stringify({
-            err_code: 1, //Wrong number of arguments
-            err_cause: `Argument not found (last_msg)`
-        }));
-    }
+    let token = checkArg(response, "token");
+    let channel_id = checkArg(response, "channel_id");
+    let last_msg = checkArg(response, "last_msg");
     chatModule.addListener(channel_id, response, last_msg);
     //Here should be no response for the request
 });
 
 app.post("/api/public_cipher", urlencodedParser, (request, response) => {
-	let count, ident;
-	count = request.body.count;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (count)`
-		}));
-	}
-	ident = request.body.ident;
-	if (login === undefined) {
-		response.status(200).send(JSON.stringify({
-			err_code: 1,
-			err_cause: `Argument not found (ident)`
-		}));
-	}
+	let count = checkArg(response, "count");
+    let ident = checkArg(response, "ident");
+    response.status(200).send("test_PUBLIC_CIPHER_method");
 });
 
 app.get("/", (request, response) => {
