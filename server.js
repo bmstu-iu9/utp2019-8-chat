f3ae0d66174489bd2e3e4505aceb2894a5cc3656
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-08-13 22:05:30 +0300

    Add https support

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const fs = require("fs");
 const process = require("process");
 const minimist = require('minimist');
+const https = require("https");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -15,8 +16,13 @@ const VERSION = "v1.0.0";
 const CONFIG_PATH = "./config.json";
 
 const defaultConfig = {
-    "default_port": 3000,
+    "default_http_port": 80,
+    "default_https_port": 433,
     "saving_interval": 60,
+    
+    "use_https": false,
+    "ssl_cert": "./crt.pem",
+    "ssl_key": "./key.pem",
 
     "mysql_host": "remotemysql.com",
     "mysql_user": "9SpT1uQOyM",
@@ -219,9 +225,20 @@ app.post("*", (request, response) => {
 
 dbModulle.load(() => {
     console.log("Data loaded");
-    const port = argv.port === undefined ? config.default_port : argv.port;
-    app.listen(port);
-    console.log(`Server started on ${port} port`);
+    if (config.use_https) {
+        const port = argv.port === undefined ? config.default_https_port : argv.port;
+        const httpsOptions = {
+            key: fs.readFileSync(config.ssl_key),
+            cert: fs.readFileSync(config.ssl_cert)
+        }        
+        https.createServer(httpsOptions, app).listen(port);
+        console.log(`Server started on ${port} port using HTTPS`);
+    }
+    else {
+        const port = argv.port === undefined ? config.default_http_port : argv.port;
+        app.listen(port);
+        console.log(`Server started on ${port} port using HTTP`);
+    }
 });
 
 
