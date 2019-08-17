e010680139e506ceda8d03d313b67f20c2caaac9
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-08-17 20:31:32 +0300

    Refactoring

--- modules/auth.js
+++ modules/auth.js
@@ -8,38 +8,20 @@ let data = [];
 let sessions = [];
 
 module.exports.register = (login, password) => {
-    if (typeof(login) !== "string" || typeof(password) !== "string") {
-        return {
-            success: false, 
-            err_code: 2, 
-            err_cause: "wrong type of argument"
-        };
-    }
     for (let i = 0; i < data.length; i++) {
         if (data[i].login === login) {
-            return {
-                success: false, 
-                err_code: 3, 
-                err_cause: "user with this login already exists"
-            };
+            return { success: false, err_code: 3, err_cause: "user with this login already exists" };
         }
     }
     data.push({
-        login: login, 
-        id: data.length + 1, 
+        login: login,
+        id: data.length + 1,
         hash: crypto.createHash("sha512").update(password).digest("base64")
     });
     return { success: true };
 }
 
 module.exports.auth = (login, password) => {
-	if (typeof(login) !== "string" || typeof(password) !== "string") {
-        return {
-            success: false, 
-            err_code: 2, 
-            err_cause: "wrong type of argument"
-        };
-    }
     let current = undefined;
     for (let i = 0; i < data.length; i++) {
         if (data[i].login === login) {
@@ -48,18 +30,10 @@ module.exports.auth = (login, password) => {
         }
     }
     if (current === undefined) {
-        return {
-            success: false, 
-            err_code: 7,
-            err_cause: "user doesn't exist"
-        };
+        return { success: false, err_code: 7, err_cause: "user doesn't exist" };
     }
     if (current.hash !== crypto.createHash("sha512").update(password).digest("base64")) {
-        return {
-            success: false, 
-            err_code: 4, 
-            err_cause: "wrong password"
-        };
+        return { success: false, err_code: 4, err_cause: "wrong password" };
     }
     let sessionKey = crypto.randomBytes(64).toString("base64");
     sessions[sessionKey] = {
@@ -67,29 +41,18 @@ module.exports.auth = (login, password) => {
         id: current.id,
         lastUpdate: new Date().getTime()
     };
-    return {
-        success: true, 
-        token: sessionKey
-    };
+    return { success: true, token: sessionKey };
 }
 
 module.exports.getUser = (token) => {
     if (sessions[token] === undefined) {
-        return {
-            success: false,
-            err_code: 5,
-            err_cause: "Wrong access token. Try to authorize again."
-        };
+        return { success: false, err_code: 5, err_cause: "Wrong access token. Try to authorize again." };
     }
     if (new Date().getTime() - sessions[token].lastUpdate > MAX_SESSION_TIME * 60000) {
-        return {
-            success: false,
-            err_code: 5,
-            err_cause: "Access token expired. Try to authorize again."
-        };
+        return { success: false, err_code: 5, err_cause: "Access token expired. Try to authorize again." };
     }
     sessions[token].lastUpdate = new Date().getTime();
-    return sessions[token].id;
+    return { success: true, userID: sessions[token].id };
 }
 
 
--- modules/database.js
+++ modules/database.js
@@ -7,146 +7,160 @@ let UsersData = [];
 let UsersChannels = [false];
 
 module.exports.get_user = (id) => {
-	if (arguments.length < 1) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-	if (typeof(id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
 	let current = UsersData[id];
-	if (typeof(current) === "undefined") return {success: false, err_code: 7, err_cause: "user doesn't exist"};
-	
-	return {success: true, user: current};
-}	
+	if (current === undefined) {
+		return { success: false, err_code: 7, err_cause: "user doesn't exist" };
+	}
+	return { success: true, user: current };
+}
 
 module.exports.change_avatar = (user_id, avatar) => {
-	if (arguments.length < 2) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-	if (typeof(user_id) !== "number" || typeof(avatar) !== "string") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-	if (UsersData[user_id] === undefined) return {success: false, err_code: 7, err_cause: "user doesn't exist"};
-	
+	if (UsersData[user_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "user doesn't exist" };
+	}
 	UsersData[user_id].avatar = avatar;
-	return {success: true};
+	return { success: true };
 }
 
 module.exports.add_to_channel = (user_id, channel_id) => {
-    if (arguments.length < 2) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-	if (typeof(user_id) !== "number" || typeof(channel_id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-	if (typeof(UsersData[user_id]) === "undefined") return {success: false, err_code: 7, err_cause: "user doesn't exist"};
-	if (typeof(UsersChannels[channel_id]) === "undefined") return {success: false, err_code: 7, err_cause: "channel doesn't exist"};
-	
+	if (UsersData[user_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "user doesn't exist" };
+	}
+	if (UsersChannels[channel_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
 	UsersData[user_id].channels.push(channel_id);
 	UsersChannels[channel_id].listeners_ids.push(user_id);
-	return {success: true};
+	return { success: true };
 }
 
 module.exports.remove_from_channel = (user_id, channel_id) => {
-    if (arguments.length < 2) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-	if (typeof(user_id) !== "number" || typeof(channel_id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-	if (typeof(UsersData[user_id]) === "undefined") return {success: false, err_code: 7, err_cause: "user doesn't exist"};
-	if (typeof(UsersChannels[channel_id]) === "undefined") return {success: false, err_code: 7, err_cause: "channel doesn't exist"};
-	
-    UsersData[user_id].channels[channel_id] = false;
+	if (UsersData[user_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "user doesn't exist" };
+	}
+	if (UsersChannels[channel_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
+	UsersData[user_id].channels[channel_id] = false;
 	for (let i = 0; i < UsersChannels[channel_id].listeners_ids.length; i++) {
 		if (UsersChannels[channel_id].listeners_ids[i] === user_id) {
 			UsersChannels[channel_id].listeners_ids[i] = false;
 			break;
 		}
 	}
-	return {success: true};
+	return { success: true };
 }
 
 module.exports.get_channel = (id) => {
-	if (arguments.length < 1) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-	if (typeof(id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
 	let current = UsersChannels[id];
-	if (typeof(current) === "undefined") return {success: false, err_code: 7, err_cause: "channel doesn't exist"};
-	
-    return {success: true, channel = current};
+	if (current === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
+	return { success: true, channel: current };
 }
 
 module.exports.create_channel = (user_id, channel_name) => {
-     if (arguments.length < 2) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-     if (typeof(user_id) !== "number" || typeof(channel_name) !== "string") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-     for (let i = 1; i < UsersChannels.length; i++) if (UsersChannels[i].name === channel_name) return {success: false, err_code: 3, err_cause: "channel with this name already exists"};
-	
-     UsersData[user_id].channels.push(UsersChannels.length);
-	 UsersChannels.push({id: UsersChannels.length, name: channel_name, owner_id: user_id, listeners_ids: {user_id}, last_message_id: undefined, last_message_time: undefined}); 
-	 return {success: true};
-}					
+	for (let i = 1; i < UsersChannels.length; i++) {
+		if (UsersChannels[i].name === channel_name) {
+			return { success: false, err_code: 3, err_cause: "channel with this name already exists" };
+		}
+	}
+	UsersData[user_id].channels.push(UsersChannels.length);
+	let newChannel = {
+		id: UsersChannels.length,
+		name: channel_name,
+		owner_id: user_id,
+		listeners_ids: { user_id },
+		last_message_id: undefined,
+		last_message_time: undefined
+	};
+	UsersChannels.push(newChannel);
+	return { success: true };
+}
 
 module.exports.channels_delete = (channel_id) => {
-    if (arguments.length < 2) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-     if (typeof(channel_id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-	if (UsersChannels[channel_id] === undefined) return {success: false, err_code: 7, err_cause: "channel doesn't exist"}; 
-	
-     for (let i = 0; i < UsersChannels[channel_id].listeners_ids.length; i++) {
-		 for (let j = 0; j < UsersData[i].channels.length; j++) {
-			 if (UsersData[i].channels[j].id === channel_id) {
-				 UsersData[i].channels[j] = false;
-				 break;
-			 }
-		 }
-	 }
-	 UsersChannels[channel_id] = false;
-	 return {success: true};
+	if (UsersChannels[channel_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
+	for (let i = 0; i < UsersChannels[channel_id].listeners_ids.length; i++) {
+		for (let j = 0; j < UsersData[i].channels.length; j++) {
+			if (UsersData[i].channels[j].id === channel_id) {
+				UsersData[i].channels[j] = false;
+				break;
+			}
+		}
+	}
+	UsersChannels[channel_id] = false;
+	return { success: true };
 }
 
 module.exports.chat_history = (channel_id, offset, count) => {
-     if (arguments.length < 3) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-     if (typeof(channel_id) !== "number" || typeof(offset) !== "number" || typeof(count) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-     if (UsersChannels[channel_id] === undefined) return {success: false, err_code: 7, err_cause: "channel doesn't exist"};
-
-    let start = 0;
-    let end = 0;
-    let len = UsersChannels[channel_id].messages.length;
-    if (offset < len) {
-        end = len - offset;
-        if (offset + count < len) start = len - offset - count;
-    }
-    //Returning number of messages and messages themselves
-    return {success: true, count: end - start, messages: UsersChannels[channel_id].messages.slice(start, end)};
+	if (UsersChannels[channel_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
+	let start = 0;
+	let end = 0;
+	let len = UsersChannels[channel_id].messages.length;
+	if (offset < len) {
+		end = len - offset;
+		if (offset + count < len)
+			start = len - offset - count;
+	}
+	return { success: true, count: end - start, messages: UsersChannels[channel_id].messages.slice(start, end) };
 }
 
-module.exports.send_message = (channel_id, message, author_id) => {
-     if (arguments.length < 3) return {success: false, err_code: 1, err_cause: "undefined arguments exist"};
-     if (typeof(channel_id) !== "number" || typeof(message) !== "string" || typeof(author_id) !== "number") return {success: false, err_code: 2, err_cause: "wrong type of argument"};
-     if (UsersChannels[channel_id] === undefined) return {success: false, err_code: 7, err_cause: "channel doesn't exist"};
-
-    UsersChannels[channel_id].messages.push({message_id: UsersChannels[channel_id].messages.length, author_id: author_id, message: message, time: new Date().getTime(), channel_id: channel_id});
-    return {success: true};
+module.exports.send_message = (channel_id, message, author_id, broadcast) => {
+	if (UsersChannels[channel_id] === undefined) {
+		return { success: false, err_code: 7, err_cause: "channel doesn't exist" };
+	}
+	let newMsg = {
+		message_id: UsersChannels[channel_id].messages.length,
+		author_id: author_id,
+		author_name: "NULL",
+		message: message,
+		time: new Date().getTime(),
+		channel_id: channel_id
+	};
+	UsersChannels[channel_id].messages.push(newMsg);
+	broadcast(channel_id, newMsg);
+	return { success: true };
 }
 
 //Information inside UsersData and UsersChannels, which accumulates during server's session,
 //saves into UsersData.json and UsersChannels.json accordingly
 module.exports.save = () => {
-    if (UsersData.length > 0) {
+	if (UsersData.length > 0) {
 		fs.writeFileSync("./Data/UsersData.json", JSON.stringify(UsersData[0]));
 		fs.appendFileSync("./Data/UsersData.json", "\n");
 	}
-    for (let i = 1; i < UsersData.length; i++) {
-        fs.appendFileSync("./Data/UsersData.json", JSON.stringify(UsersData[i]));
+	for (let i = 1; i < UsersData.length; i++) {
+		fs.appendFileSync("./Data/UsersData.json", JSON.stringify(UsersData[i]));
 		if (i < UsersData.length - 1) fs.appendFileSync("./Data/UsersData.json", "\n");
-    }
-    if (UsersChannels.length > 0) {
+	}
+	if (UsersChannels.length > 0) {
 		fs.writeFileSync("./Data/UsersChannels.json", JSON.stringify(UsersChannels[0]));
 		fs.appendFileSync("./Data/UsersChannels.json", "\n");
 	}
-    for (let i = 1; i < UsersChannels.length; i++) {
-        fs.appendFileSync("./Data/UsersChannels.json", JSON.stringify(UsersChannels[i]));
+	for (let i = 1; i < UsersChannels.length; i++) {
+		fs.appendFileSync("./Data/UsersChannels.json", JSON.stringify(UsersChannels[i]));
 		if (i < UsersChannels.length - 1) fs.appendFileSync("./Data/UsersChannels.json", "\n");
-    }
+	}
 }
 
 //Loading information, that had been recording during previous server's sessions,
 //from UsersData.json and UsersChannels.json to UsersData and UsersChannels respectively
 module.exports.load = (callback) => {
-    let current = readline.createInterface({input: fs.createReadStream("./Data/UsersData.json")});
-    current.on('line', (line) => {
-        if (line.length > 5) UsersData.push(JSON.parse(line));
-    });
-    current.on('close', () => {
-        let current_1 = readline.createInterface({input: fs.createReadStream("./Data/UsersChannels.json")});
-        current_1.on('line', (line) => {
-            if (line.length > 5) UsersChannels.push(JSON.parse(line));
-        });
-        current_1.on('close', () => {
-            callback();
-        });
-    });
+	let current = readline.createInterface({ input: fs.createReadStream("./Data/UsersData.json") });
+	current.on('line', (line) => {
+		if (line.length > 5) UsersData.push(JSON.parse(line));
+	});
+	current.on('close', () => {
+		let current_1 = readline.createInterface({ input: fs.createReadStream("./Data/UsersChannels.json") });
+		current_1.on('line', (line) => {
+			if (line.length > 5) UsersChannels.push(JSON.parse(line));
+		});
+		current_1.on('close', () => {
+			callback();
+		});
+	});
 }
