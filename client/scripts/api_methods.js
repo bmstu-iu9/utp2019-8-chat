a9e0b5777fade382582efadc273c269ff92589c0
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-08-26 23:14:07 +0300

    api_methods.js refactoring

--- client/scripts/api_methods.js
+++ client/scripts/api_methods.js
@@ -1,235 +1,99 @@
 'use strict'
 
-// api/get_user с визуальным интерфейсом
-
-let timer1;
-
-const show_user_data = () => {
-    if (id > 0) {
-        let sendStr = "id=" + userId;
-        send(adress + "api/get_user", sendStr);
-        timer1 = setInterval(function () {
-            if (time < 1500)
-                time *= 2;
-            else clearInterval(timer1);
-            let r = get();
-            if (r != "") {
-                let responseObj = JSON.parse(r);
-                processShow_user_data(responseObj);
-                clearInterval(timer1);
-            }
-        }, time);
-    }
-};
-
-// api/get_user
-
-const apiGetUserData = () => {
-    let sendStr = "id=" + userId;
-    send(adress + "api/get_user", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiGetUserData(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
+const API_request = (path, params) => {
+    return new Promise((resolve, reject) => {
+        request(path, params)
+            .then((res) => {
+                const response = JSON.parse(res.response);
+                if (response.success)
+                    return resolve(response.token);
+                else
+                    return reject(response.err_cause);
+            })
+            .catch((err) => {
+                return reject(err);
+            });
+    });
 }
-
-// api/add_to_channel
-
-const apiAddToChannel = (channel_id) => {
-    let sendStr = "token=" + token + "&user_id=" + userId + "&channel_id=" + channel_id;
-    send(adress + "api/add_to_channel", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiAddToChannel(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/remove_from_channel
-
-const apiRemoveFromChannel = (channel_id) => {
-    let sendStr = "token=" + token + "&user_id=" + userId + "&channel_id=" + channel_id;
-    send(adress + "api/remove_from_channel", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiRemoveFromChannel(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/change_avatar
-
-const apiChangeAvatar = (avatar) => {
-    let sendStr = "token=" + token + "&user_id=" + userId + "&avatar=" + avatar;
-    send(adress + "api/change_avatar", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiChangeAvatar(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/get_channel
-
-const apiGetChannel = (channel_id) => {
-    let sendStr = "id=" + channel_id;
-    send(adress + "api/get_channel", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiGetChannel(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/create_channel
-
-const apiCreateChannel = (channel_name) => {
-    let sendStr = "token=" + token + "&channel_name=" + channel_name;
-    send(adress + "api/create_channel", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiCreateChannel(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/delete_channel
-
-const apiDeleteChannel = (ch_id) => {
-    let sendStr = "token=" + token + "&channel_id=" + ch_id;
-    send(adress + "api/delete_channel", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiDeleteChannel(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/get_messages
-
-const apiGetMessages = (ch_id, offset, count) => {
-    let sendStr = "token=" + token + "&channel_id=" + ch_id + "&offset=" + offset + "&count=" + count;
-    send(adress + "api/get_messages", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiGetMessage(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-// api/send_message
-
-const apiSendMessage = (ch_id, msg) => {
-    let sendStr = "token=" + token + "&channel_id=" + ch_id + "&message=" + msg;
-    send(adress + "api/send_message", sendStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiSendMessage(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-
-// api/exit_session
 
 const apiExitSession = () => {
-    let paramsStr = "token=" + token;
-    send(adress + "api/exit_session", paramsStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiExitSession(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
+    const params = { token: getCookie("accessToken"), };
+    return API_request("api/exit_session", params);
 }
 
-// api/exit_all_session
-
 const apiExitAllSessions = () => {
-    let paramsStr = "token=" + token;
-    send(adress + "api/exit_all_session", paramsStr);
-    let ltime = time;
-    let timer = setInterval(function () {
-        if (ltime < 1500)
-            ltime *= 2;
-        else clearInterval(timer);
-        let r = get();
-        if (r != "") {
-            let responseObj = JSON.parse(r);
-            processApiExitAllSessions(responseObj);
-            clearInterval(timer);
-        }
-    }, ltime);
+    const params = { token: getCookie("accessToken"), };
+    return API_request("api/exit_all_session", params);
+}
+
+const apiGetUser = (id) => {
+    const params = { id: id };
+    return API_request("api/get_user", params);
+}
+
+const apiAddToChannel = (user_id, channel_id) => {
+    const params = {
+        token: getCookie("accessToken"),
+        user_id: user_id,
+        channel_id: channel_id
+    };
+    return API_request("api/add_to_channel", params);
+}
+
+const apiRemoveFromChannel = (user_id, channel_id) => {
+    const params = {
+        token: getCookie("accessToken"),
+        user_id: user_id,
+        channel_id: channel_id
+    };
+    return API_request("api/remove_from_channel", params);
+}
+
+const apiChangeAvatar = (user_id, avatar) => {
+    const params = {
+        token: getCookie("accessToken"),
+        user_id: user_id,
+        avatar: avatar
+    };
+    return API_request("api/change_avatar", params);
+}
+
+const apiGetChannel = (channel_id) => {
+    const params = { id: channel_id };
+    return API_request("api/get_channel", params);
+}
+
+const apiCreateChannel = (name) => {
+    const params = {
+        token: getCookie("accessToken"),
+        channel_name: name
+    };
+    return API_request("api/create_channel", params);
+}
+
+const apiDeleteChannel = (ch_id) => {
+    const params = {
+        token: getCookie("accessToken"),
+        channel_id: ch_id
+    };
+    return API_request("api/delete_channel", params);
+}
+
+const apiGetMessages = (ch_id, offset, count) => {
+    const params = {
+        token: getCookie("accessToken"),
+        channel_id: ch_id,
+        offset: offset,
+        count: count
+    };
+    return API_request("api/get_messages", params);
+}
+
+const apiSendMessage = (ch_id, msg) => {
+    const params = {
+        token: getCookie("accessToken"),
+        channel_id: ch_id,
+        message: msg,
+    };
+    return API_request("api/send_message", params);
 }
