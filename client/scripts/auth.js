4c80c0cc9ae90b03d0d488de4cc608809729292b
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-09-01 14:38:02 +0300

    Refactoring in the auth forms

--- client/scripts/auth.js
+++ client/scripts/auth.js
@@ -3,23 +3,21 @@
 apiCheckToken()
     .then(res => {
         console.log("Redirect");
-        window.location.replace('/index.html');
+        window.location.replace('/index.html'); //???
     })
-    .catch(err => {});
+    .catch(err => { });
 
 const auth = (login, password) => {
     return new Promise((resolve, reject) => {
         request("api/auth", { login: login, password: password })
-            .then((res) => {
+            .then(res => {
                 const response = JSON.parse(res.response);
                 if (response.success)
                     return resolve(response.token);
                 else
                     return reject(response.err_cause);
             })
-            .catch((err) => {
-                return reject(err);
-            });
+            .catch(err => reject(err));
     });
 }
 
@@ -28,13 +26,11 @@ const process = () => {
     const password = document.getElementById("password").value;
     if (checkLogin(login) && checkPassword(password)) {
         auth(login, password)
-            .then((res) => {
+            .then(res => {
                 setCookie("accessToken", res);
                 window.location.replace('/index.html');
             })
-            .catch((err) => {
-                alert(err);
-            });
+            .catch(err => { alert(err); });
     }
     else {
         alert("Неверный логин или пароль");
@@ -42,11 +38,16 @@ const process = () => {
 }
 
 document.getElementById("authBtn").addEventListener('click', process);
-document.getElementById("login").addEventListener('keyup', (e) => {
-    if (e.key === 'Enter')
-        process();
+document.getElementById("login").addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
+        const pass_box = document.getElementById("password");
+        if (pass_box.value !== "")
+            pass_box.focus();
+        else
+            process();
+    }
 });
-document.getElementById("password").addEventListener('keyup', (e) => {
+document.getElementById("password").addEventListener('keyup', e => {
     if (e.key === 'Enter')
         process();
 });
